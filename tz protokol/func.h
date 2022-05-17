989c9b9baeb53754f61ef4485fbe813dd85034e2
anchous0215 <100085713+anchous0215@users.noreply.github.com>
2022-05-18 00:06:20 +0300

    Update func.h

--- tz protokol/func.h
+++ tz protokol/func.h
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
+#include <time.h>
+#include <sys/times.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <openssl/md5.h>
 #include <openssl/sha.h>
 #include <openssl/aes.h>
 #include <openssl/des.h>
 
-int len_find(char *type_c, char *type_h, unsigned char *c, unsigned char *h, int *block_size);
-int def_len(unsigned char type_h, unsigned char type_c, char *hash, char *cipher, int *block_size);
-int check(char *code, char hash, char cipher);
 void my_free(void *pointer);
+int len_find(char *type_c, char *type_h, unsigned char *c, unsigned char *h);
+int def_len(unsigned char type_h, unsigned char type_c, char *hash, char *cipher);
+int check(char *code, char hash, char cipher);
 int compare(unsigned char *s1);
 unsigned char *ASCII_to_hhx(char *s, unsigned char *x);
 unsigned char *int_to_hhx(unsigned long int in, unsigned char *x);
@@ -27,4 +29,5 @@ void aes192_cbc_decrypt(unsigned char *in, size_t in_len, unsigned char *iv, uns
 void aes192_cbc_encrypt(unsigned char *in, size_t in_len, unsigned char *iv, unsigned char *key, unsigned char *out);
 void aes256_cbc_decrypt(unsigned char *in, size_t in_len, unsigned char *iv, unsigned char *key, unsigned char *out);
 void aes256_cbc_encrypt(unsigned char *in, size_t in_len, unsigned char *iv, unsigned char *key, unsigned char *out);
-void help();
+unsigned char *encrypt_text(unsigned char *in, size_t in_len, unsigned char *iv, unsigned char *key, unsigned char type_c, unsigned char *cipher_text);//
+unsigned char *decrypt_text(unsigned char *in, size_t in_len, unsigned char *iv, unsigned char *key, unsigned char type_c, unsigned char *open_text);//
